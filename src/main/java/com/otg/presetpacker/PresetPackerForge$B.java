85a1428ed3366dab95d28b5c7742bcc56c44c9a4
audun <audunwo@uio.no>
2021-01-27 18:33:25 +0100

    Final touches, should now work with OTG 1.16.4 0.0.4+

--- src/main/java/com/otg/presetpacker/PresetPackerForge$B.java
+++ src/main/java/com/otg/presetpacker/PresetPackerForge$B.java
@@ -1,10 +1,5 @@
 package com.otg.presetpacker;
 
-import net.minecraft.block.Block;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.Level;
@@ -17,13 +12,11 @@ import java.util.jar.JarFile;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("${presetpackerid}")
-public class PresetPackerForge$B
-{
+public class PresetPackerForge$B {
     public PresetPackerForge$B() {
         // Directly reference a log4j logger.
         Logger logger = LogManager.getLogger("${presetpackerid}");
-        try
-        {
+        try {
             String presetFolderPath = new File(".").getCanonicalPath() + File.separator + "config" + File.separator + "OpenTerrainGenerator" + File.separator + "Presets" + File.separator;
 
             logger.log(Level.INFO, "Starting extraction of ${modDisplayName}");
@@ -31,10 +24,11 @@ public class PresetPackerForge$B
             JarFile jarFile = new JarFile(ModList.get().getModFileById("${presetpackerid}").getFile().getFilePath().toFile());
             int filesWritten = PresetUnpackUtil.extractPreset(jarFile, presetFolderPath);
             jarFile.close();
-            logger.log(Level.INFO, "Preset ${modDisplayName} extracted, wrote {} files", filesWritten);
-        }
-        catch (IOException e)
-        {
+            if (filesWritten > 0)
+                logger.log(Level.INFO, "Preset ${modDisplayName} extracted, wrote {} files", filesWritten);
+            else
+                logger.log(Level.INFO, "Preset ${modDisplayName} was already up to date, skipped");
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
