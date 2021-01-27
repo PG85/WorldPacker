1743f2124b2740c9205dac6935ce08a0fe30f322
audun <audunwo@uio.no>
2021-01-27 16:24:18 +0100

    Replaced IMC with dependency ordering

--- src/main/java/com/otg/presetpacker/PresetPackerForge$B.java
+++ src/main/java/com/otg/presetpacker/PresetPackerForge$B.java
@@ -7,8 +7,6 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -19,22 +17,9 @@ import java.util.jar.JarFile;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("${presetpackerid}")
-@Mod.EventBusSubscriber(modid = "${presetpackerid}", bus = Mod.EventBusSubscriber.Bus.MOD)
 public class PresetPackerForge$B
 {
     public PresetPackerForge$B() {
-        // Register the setup method for modloading
-        //FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-
-        // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    // We use the Register Blocks event to ensure we unpack files before OTG starts loading biomes
-    // The block registry is the first event to fire, which is why we specifically use this event
-    @SubscribeEvent
-    public static void setup(RegistryEvent.Register<Block> event)
-    {
         // Directly reference a log4j logger.
         Logger logger = LogManager.getLogger("${presetpackerid}");
         try
@@ -46,10 +31,6 @@ public class PresetPackerForge$B
             JarFile jarFile = new JarFile(ModList.get().getModFileById("${presetpackerid}").getFile().getFilePath().toFile());
             int filesWritten = PresetUnpackUtil.extractPreset(jarFile, presetFolderPath);
             jarFile.close();
-            for (String s : PresetUnpackUtil.versionedPresetNames.keySet())
-            {
-                InterModComms.sendTo("otg", "loadNewPreset", () -> s);
-            }
             logger.log(Level.INFO, "Preset ${modDisplayName} extracted, wrote {} files", filesWritten);
         }
         catch (IOException e)
