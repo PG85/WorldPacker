0f1119e8fe29600d52366a877eceedcffeb7438f
audun <audunwo@uio.no>
2021-01-27 11:26:57 +0100

    Fixed preset name not replacing properly into Mods.toml

--- src/main/java/com/otg/presetpacker/PresetPackerSpigot$B.java
+++ src/main/java/com/otg/presetpacker/PresetPackerSpigot$B.java
@@ -6,19 +6,22 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.io.File;
 import java.io.IOException;
 import java.util.jar.JarFile;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PresetPackerSpigot$B extends JavaPlugin {
     @Override
     public void onEnable()
     {
-        java.util.logging.Logger logger = Bukkit.getLogger();
+        Logger logger = Bukkit.getLogger();
         try
         {
             String presetFolderPath = new File(".").getCanonicalPath() + File.separator + "plugins" + File.separator + "OpenTerrainGenerator" + File.separator + "Presets" + File.separator;
-            logger.log(java.util.logging.Level.INFO, "Extracting ${displayName}");
+            logger.log(Level.INFO, "Starting extraction of ${modDisplayName}");
             JarFile jarFile = new JarFile(new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().getFile()));
             PresetUnpackUtil.extractPreset(jarFile, presetFolderPath);
             jarFile.close();
+            logger.log(Level.INFO, "Preset ${modDisplayName} extracted");
         }
         catch (IOException e)
         {
