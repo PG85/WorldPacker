4aeb184266a769a631d2867570ffc04618839aaf
FrankTCA <frank@steamedveggie.com>
2021-08-11 11:17:21 -0500

    Add proper preset version handling

--- src/main/java/com/otg/presetpacker/PresetPackerForge$B.java
+++ src/main/java/com/otg/presetpacker/PresetPackerForge$B.java
@@ -1,5 +1,11 @@
 package com.otg.presetpacker;
 
+import net.minecraft.client.gui.screen.MainMenuScreen;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraftforge.client.event.GuiOpenEvent;
+import net.minecraftforge.common.ForgeConfig;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.Level;
@@ -12,7 +18,11 @@ import java.util.jar.JarFile;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("${presetpackerid}")
+@Mod.EventBusSubscriber
 public class PresetPackerForge$B {
+
+    public static GuiOpenEvent guiOpenEvent;
+
     public PresetPackerForge$B() {
         // Directly reference a log4j logger.
         Logger logger = LogManager.getLogger("${presetpackerid}");
@@ -22,7 +32,8 @@ public class PresetPackerForge$B {
             logger.log(Level.INFO, "Starting extraction of ${modDisplayName}");
             // Fetch the world files from this mod's own jar
             JarFile jarFile = new JarFile(ModList.get().getModFileById("${presetpackerid}").getFile().getFilePath().toFile());
-            int filesWritten = PresetUnpackUtil.extractPreset(jarFile, presetFolderPath);
+            boolean isServer = ForgeConfig.CLIENT == null;
+            int filesWritten = PresetUnpackUtil.extractPreset(jarFile, presetFolderPath, isServer);
             jarFile.close();
             if (filesWritten > 0)
                 logger.log(Level.INFO, "Preset ${modDisplayName} extracted, wrote {} files", filesWritten);
@@ -30,6 +41,15 @@ public class PresetPackerForge$B {
                 logger.log(Level.INFO, "Preset ${modDisplayName} was already up to date, skipped");
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    @SubscribeEvent
+    public static void onGuiOpen(GuiOpenEvent evt) {
+        if (evt.getGui() instanceof MainMenuScreen && PresetUnpackUtil.requiresErrorScreen) {
+            guiOpenEvent = evt;
+            Screen mainMenuScreen = evt.getGui();
+            evt.setGui(new ClientOutdatedPresetScreen(new StringTextComponent("Preset Update"), mainMenuScreen));
         }
     }
 }
