fe7ed6de1440f0686d7c0fc6b339dff1b8098047
FrankTCA <frank@steamedveggie.com>
2022-02-25 23:10:05 -0500

    Take 1 at fixing for 1.18

--- src/main/java/com/otg/presetpacker/$presetpackerid/ClientOutdatedPresetScreen$B.java
+++ src/main/java/com/otg/presetpacker/$presetpackerid/ClientOutdatedPresetScreen$B.java
@@ -1,12 +1,12 @@
 package com.otg.presetpacker.$presetpackerid;
 
-import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.util.text.TextFormatting;
-import net.minecraftforge.fml.client.gui.widget.ExtendedButton;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
 
 import java.io.File;
 import java.io.IOException;
@@ -17,9 +17,9 @@ public class ClientOutdatedPresetScreen$B extends Screen {
     private final int oldVersion;
     private final int newVersion;
     private final Path presetPath;
-    private ITextComponent errorHeader;
+    private Component errorHeader;
 
-    protected ClientOutdatedPresetScreen$B(ITextComponent titleIn, String presetName, int oldVersion, int newVersion, Path presetPath) {
+    protected ClientOutdatedPresetScreen$B(Component titleIn, String presetName, int oldVersion, int newVersion, Path presetPath) {
         super(titleIn);
         this.presetName = presetName;
         this.oldVersion = oldVersion;
@@ -30,55 +30,54 @@ public class ClientOutdatedPresetScreen$B extends Screen {
     @Override
     public void init() {
         super.init();
-        this.buttons.clear();
-        this.children.clear();
-        this.errorHeader = new StringTextComponent(TextFormatting.RED + "Tried to update \"" + presetName
-                + "\" from v" + oldVersion + " to " + newVersion + TextFormatting.RESET);
+        this.renderables.clear();
+        this.errorHeader = new TextComponent(ChatFormatting.RED + "Tried to update \"" + presetName
+                + "\" from v" + oldVersion + " to " + newVersion + ChatFormatting.RESET);
         int yOffset = 46;
 
-        this.addButton(
-                new ExtendedButton(
+        this.addRenderableWidget(
+                new Button(
                         10,
                         this.height - yOffset,
                         this.width / 3 - 15,
                         20,
-                        new StringTextComponent("Update and restart"),
+                        new TextComponent("Update and restart"),
                         b -> {
                             try {
                                 File markerFile = presetPath.resolve(".REPLACE_ME").toFile();
                                 if (!markerFile.exists()) {
                                     markerFile.createNewFile();
                                 }
-                                Minecraft.getInstance().shutdownMinecraftApplet();
+                                Minecraft.getInstance().close();
                             } catch (IOException e) {
                                 e.printStackTrace();
                             }
                         }
         ));
 
-        this.addButton(
-                new ExtendedButton(
+        this.addRenderableWidget(
+                new Button(
                         (this.width / 3)+5,
                         this.height - yOffset,
                         this.width / 3 - 15,
                         20,
-                        new StringTextComponent("Ignore for now"),
-                        b -> this.closeScreen()));
+                        new TextComponent("Ignore for now"),
+                        b -> this.clearWidgets()));
 
-        this.addButton(
-                new ExtendedButton(
+        this.addRenderableWidget(
+                new Button(
                         5+(this.width / 3 )*2,
                         this.height - yOffset,
                         this.width / 3 - 15,
                         20,
-                        new StringTextComponent("Ignore this update"),
+                        new TextComponent("Ignore this update"),
                         b -> {
                             try {
                                 File markerFile = presetPath.resolve(".DENY_PRESET_UPDATE").toFile();
                                 if (!markerFile.exists()) {
                                     markerFile.createNewFile();
                                 }
-                                this.closeScreen();
+                                this.clearWidgets();
                             } catch (IOException e) {
                                 e.printStackTrace();
                             }
@@ -86,12 +85,14 @@ public class ClientOutdatedPresetScreen$B extends Screen {
         ));
     }
 
+
+
     @Override
-    public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
+    public void render(PoseStack matrixStack, int mouseX, int mouseY, float partialTicks) {
         this.renderBackground(matrixStack);
-        font.drawString(matrixStack, "Preset Update", (this.width >> 1) - 50, 25, 0xFFFFFF);
-        font.drawString(matrixStack, this.errorHeader.getString(), (this.width >> 1) - 110, 70, 0xFF0000);
-        font.drawString(matrixStack, "Update may break preexisting OTG worlds using "+ presetName +".", (this.width >> 1) - 150, 90, 0xFF0000);
-        this.buttons.forEach(button -> button.render(matrixStack, mouseX, mouseY, partialTicks));
+        font.draw(matrixStack, "Preset Update", (this.width >> 1) - 50, 25, 0xFFFFFF);
+        font.draw(matrixStack, this.errorHeader.getString(), (this.width >> 1) - 110, 70, 0xFF0000);
+        font.draw(matrixStack, "Update may break preexisting OTG worlds using "+ presetName +".", (this.width >> 1) - 150, 90, 0xFF0000);
+        this.renderables.forEach(button -> button.render(matrixStack, mouseX, mouseY, partialTicks));
     }
 }
