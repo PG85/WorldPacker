6a4625a75b8944be0f9d1595efa924fdb73b0a5e
PG85 <rik_lubking@hotmail.com>
2022-03-15 18:52:02 +0100

    Revert Frank's remaining commits

--- src/main/java/com/otg/presetpacker/$presetpackerid/ClientOutdatedPresetScreen$B.java
+++ src/main/java/com/otg/presetpacker/$presetpackerid/ClientOutdatedPresetScreen$B.java
@@ -1,23 +1,10 @@
 package com.otg.presetpacker.$presetpackerid;
 
-import com.mojang.blaze3d.matrix.MatrixStack;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.util.text.TextFormatting;
-import net.minecraftforge.fml.client.gui.widget.ExtendedButton;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Path;
-
 public class ClientOutdatedPresetScreen$B extends Screen {
     private final String presetName;
     private final int oldVersion;
     private final int newVersion;
     private final Path presetPath;
-    private ITextComponent errorHeader;
 
     protected ClientOutdatedPresetScreen$B(ITextComponent titleIn, String presetName, int oldVersion, int newVersion, Path presetPath) {
         super(titleIn);
@@ -25,73 +12,5 @@ public class ClientOutdatedPresetScreen$B extends Screen {
         this.oldVersion = oldVersion;
         this.newVersion = newVersion;
         this.presetPath = presetPath;
-    }
-
-    @Override
-    public void init() {
-        super.init();
-        this.buttons.clear();
-        this.children.clear();
-        this.errorHeader = new StringTextComponent(TextFormatting.RED + "Tried to update \"" + presetName
-                + "\" from v" + oldVersion + " to " + newVersion + TextFormatting.RESET);
-        int yOffset = 46;
-
-        this.addButton(
-                new ExtendedButton(
-                        10,
-                        this.height - yOffset,
-                        this.width / 3 - 15,
-                        20,
-                        new StringTextComponent("Update and restart"),
-                        b -> {
-                            try {
-                                File markerFile = presetPath.resolve(".REPLACE_ME").toFile();
-                                if (!markerFile.exists()) {
-                                    markerFile.createNewFile();
-                                }
-                                Minecraft.getInstance().shutdownMinecraftApplet();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-        ));
-
-        this.addButton(
-                new ExtendedButton(
-                        (this.width / 3)+5,
-                        this.height - yOffset,
-                        this.width / 3 - 15,
-                        20,
-                        new StringTextComponent("Ignore for now"),
-                        b -> this.closeScreen()));
-
-        this.addButton(
-                new ExtendedButton(
-                        5+(this.width / 3 )*2,
-                        this.height - yOffset,
-                        this.width / 3 - 15,
-                        20,
-                        new StringTextComponent("Ignore this update"),
-                        b -> {
-                            try {
-                                File markerFile = presetPath.resolve(".DENY_PRESET_UPDATE").toFile();
-                                if (!markerFile.exists()) {
-                                    markerFile.createNewFile();
-                                }
-                                this.closeScreen();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-        ));
-    }
-
-    @Override
-    public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
-        this.renderBackground(matrixStack);
-        font.drawString(matrixStack, "Preset Update", (this.width >> 1) - 50, 25, 0xFFFFFF);
-        font.drawString(matrixStack, this.errorHeader.getString(), (this.width >> 1) - 110, 70, 0xFF0000);
-        font.drawString(matrixStack, "Update may break preexisting OTG worlds using "+ presetName +".", (this.width >> 1) - 150, 90, 0xFF0000);
-        this.buttons.forEach(button -> button.render(matrixStack, mouseX, mouseY, partialTicks));
     }
 }
--- src/main/java/com/otg/presetpacker/$presetpackerid/PresetPackerForge$B.java
+++ src/main/java/com/otg/presetpacker/$presetpackerid/PresetPackerForge$B.java
@@ -1,26 +1,7 @@
 package com.otg.presetpacker.$presetpackerid;
 
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraft.client.gui.screen.MainMenuScreen;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.util.text.StringTextComponent;
-import net.minecraftforge.client.event.GuiOpenEvent;
-import net.minecraftforge.common.ForgeConfig;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModList;
-import net.minecraftforge.fml.common.Mod;
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.jar.JarFile;
-
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("${presetpackerid}")
-@Mod.EventBusSubscriber
 public class PresetPackerForge$B {
     private static PresetUnpackUtil$B util;
 
@@ -33,9 +14,8 @@ public class PresetPackerForge$B {
             logger.log(Level.INFO, "Starting extraction of ${modDisplayName}");
             // Fetch the world files from this mod's own jar
             JarFile jarFile = new JarFile(ModList.get().getModFileById("${presetpackerid}").getFile().getFilePath().toFile());
-            boolean isServer = ForgeConfig.CLIENT == null;
             util = new PresetUnpackUtil$B();
-            int filesWritten = util.extractPreset(jarFile, presetFolderPath, isServer);
+            int filesWritten = util.extractPreset(jarFile, presetFolderPath);
             jarFile.close();
             if (filesWritten > 0)
                 logger.log(Level.INFO, "Preset ${modDisplayName} extracted, wrote "+filesWritten+" files");
